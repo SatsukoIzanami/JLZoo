bac6b7cd3d77cf937496d45eea490b1d7a71ddd4
SatsukoIzanami <satsukoizanami@gmail.com>
2025-10-05 15:48:46 -0500

    Update config.js

--- components/config.js
+++ components/config.js
@@ -1,4 +1,5 @@
 // components/config.js
 export const API_BASE = location.hostname.endsWith('github.io')
-  ? 'https://your-zoo-api.onrender.com/api'
+  ? 'https://jlzoo.onrender.com/api'
   : 'http://localhost:3000/api';
+
